dcfd7f2ba98ff19ecddfebe5ac87e82f9baffbf3
tommykorch <tkorchmit@bk.ru>
2025-12-14 19:14:22 +0700

    Gesture click recognition

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +30,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             GestureDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,10 +41,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen(modifier: Modifier = Modifier) {
+    ClickDemo(modifier)
+
+}
+@Composable
+fun ClickDemo(modifier: Modifier = Modifier) {
+    var colorState by remember { mutableStateOf(true)}
+    var bgColor by remember { mutableStateOf(Color.Blue) }
+    val clickHandler = {
+        colorState = !colorState
+        bgColor = if (colorState) {
+            Color.Blue
+        } else {
+            Color.DarkGray
+        }
+    }
+    Box(
+        modifier
+            .clickable { clickHandler() }
+            .background(bgColor)
+            .size(100.dp)
     )
 }
 
@@ -42,6 +69,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GestureDemoTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
